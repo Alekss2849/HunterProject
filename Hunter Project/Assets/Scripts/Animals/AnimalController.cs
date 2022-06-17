e23d7dd1d613b92f904a82548c7aa9a49fbffcfe
Aleks2849 <alekss2849@ukr.net>
2022-06-17 22:47:59 +0300

    * Wolf refactoring
    + Borders escape fix
    + Additive behaviour

--- Hunter Project/Assets/Scripts/Animals/AnimalController.cs
+++ Hunter Project/Assets/Scripts/Animals/AnimalController.cs
@@ -32,14 +32,11 @@ namespace HunterProject.Animals
             
             for (int i = 0; i != enemyPositions.Length; ++i)
             {
-                Debug.DrawLine(currentPos, enemyPositions[i], Color.red);
                 var direction = enemyPositions[i] - currentPos;
 
                 sum += direction.normalized;
             }
             
-            Debug.DrawLine(currentPos, currentPos - sum * speed, Color.cyan);
-
             return currentPos - sum * speed;
         }
 
--- Hunter Project/Assets/Scripts/Animals/UnityContext/WolfUnity.cs
+++ Hunter Project/Assets/Scripts/Animals/UnityContext/WolfUnity.cs
@@ -1,5 +1,6 @@
 ﻿using HunterProject.Animals;
 using HunterProject.Animals.Data;
+using HunterProject.Data;
 using UnityEngine;
 
 namespace DefaultNamespace
@@ -10,46 +11,29 @@ namespace DefaultNamespace
         private MovementProperties _movementProperties;
 
         [SerializeField]
-        private float _searchDistance;
+        private float _walkDistance;
 
         private WolfController _wolfController;
         
-        private const string _BORDER_TAG_ = "Border";
-        private const string _WOLF_TAG_ = "Wolf";
-
         public void Start()
         {
-            _wolfController = new WolfController(transform, _movementProperties, _searchDistance);
-            _wolfController.UpdateState();
+            _wolfController = new WolfController(transform, _movementProperties, _walkDistance);
         }
 
         private void Update()
         {
-            _wolfController.UpdateState();
-            transform.position += _wolfController.GetSteeringVelocity(transform.position) * Time.deltaTime;
+           _wolfController.Update();
         }
-        
-        //
-        // private void OnCollisionEnter2D(Collision2D collision)
-        // {
-        //     if(collision.gameObject.CompareTag(_BORDER_TAG_) || collision.gameObject.CompareTag(_WOLF_TAG_))
-        //     {
-        //         return;
-        //     }
-        //     
-        //     Destroy(collision.gameObject);
-        //     _wolfController.UpdateState();
-        // }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if(other.gameObject.CompareTag(_BORDER_TAG_) || other.gameObject.CompareTag(_WOLF_TAG_))
+            if(other.gameObject.CompareTag(Idents._WOLF_TAG) || other.gameObject.CompareTag(Idents._BORDER_TAG))
             {
                 return;
             }
             
             Destroy(other.gameObject);
-            _wolfController.UpdateState();
+            _wolfController?.UpdateState(transform);
         }
     }
 }
